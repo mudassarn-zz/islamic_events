44bdb4e034174070ccfdc94cbc9b23fbf9a16100
Muhammad Mudassar Nazar <mudassarn@gmail.com>
2013-09-07 09:16:12 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IslamicEvents::Application.config.secret_token = '66870bb4bb27a12986f379925b052fedc98225548482bf928f1264e4116f34f24cc69a402e15abbf4f6a69607fd8846bfd4b8b2867dfcdbad23515fe5c200125'
