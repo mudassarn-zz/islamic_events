37666ba7b3d4c2f8ca096ff46071e2aced627a95
Muhammad Mudassar Nazar <mudassarn@gmail.com>
2013-09-07 23:53:35 -0400

    Added basic tests for Speaker Model

--- spec/models/speaker_spec.rb
+++ spec/models/speaker_spec.rb
@@ -1,5 +1,41 @@
 require 'spec_helper'
 
 describe Speaker do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @speaker = Speaker.new
+    @speaker.name="Mudassar"
+    @speaker.profile="This is a test profile"
+  end
+
+  subject {@speaker}
+
+
+  it { should respond_to(:name) }
+  it { should respond_to(:profile) }
+
+
+  describe "when name and profile both are not blank" do
+    it {should be_valid}
+  end
+
+  describe "when name is blank" do
+    before { @speaker.name = " "}
+    it {should_not be_valid}
+  end
+
+  describe "when name contains numeric value" do
+    before { @speaker.name = " Mudassar 123 Ab"}
+    it {should_not be_valid}
+  end
+
+  describe "when name contains special character value" do
+    before { @speaker.name = " Mudassar _ Ab"}
+    it {should_not be_valid}
+  end
+
+  describe "when profile is blank" do
+    before { @speaker.profile = " "}
+    it {should_not be_valid}
+  end
+
 end
